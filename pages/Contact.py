a69d3d6a2b7c152875608fdf36085eaa1d434e8a
Jeremy Abraham <jeremyabraham17@gmail.com>
2023-11-10 13:50:38 -0500

    Connecting Sending emails to Contact form

--- pages/Contact.py
+++ pages/Contact.py
@@ -1,13 +1,22 @@
 import streamlit as st 
+from send_email import send_email
 
 st.header("Contact Me")
 
 with st.form(key="my_form"):
     user_email = st.text_input("Your Email Address")
-    message = st.text_area("Your message")
+    new_message = st.text_area("Your message")
+    message = f"""\
+        Subject: New Mail from {user_email}
+        From: {user_email}
+        {new_message}
+        """
+    
     button = st.form_submit_button("Submit")
     if button:
-        print("Submitted")
+        send_email(message)
+        st.info("Your Email was sent successfully!")
+        
 
 
 
